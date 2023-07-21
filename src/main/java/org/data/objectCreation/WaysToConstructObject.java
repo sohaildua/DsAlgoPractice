5ee0c28dacb0b420f18056a68b0e33859a1f7966
sdgamer007 <sohaildua38@gmail.com>
2023-07-21 23:43:56 +0200

    sorting algos

---
+++ src/main/java/org/data/objectCreation/WaysToConstructObject.java
@@ -0,0 +1,35 @@
+package org.data.objectCreation;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+public class WaysToConstructObject {
+    private int x;
+    private String y;
+
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        // new method
+        WaysToConstructObject waysToConstructObject = new WaysToConstructObject();
+
+        //new Instance method
+       /* Class b = Class.forName("waysToConstructObject");
+        WaysToConstructObject waysToConstructObject1 = (WaysToConstructObject) b.newInstance();
+
+        //new Instance method constructor
+        Class c = Class.forName("waysToConstructObject");
+        //WaysToConstructObject waysToConstructObject2 = (WaysToConstructObject)c.getConstructor().newInstance();*/
+
+        // clone
+
+        List<Integer> myList = Arrays.asList(10,15,8,49,25,98,32);
+        myList.forEach(System.out::println);
+        Map<Integer, String> namesMap = new HashMap<>();
+        namesMap.put(1, "Larry");
+        namesMap.put(2, "Steve");
+        namesMap.put(3, "James");
+
+        List<String> stringList = new ArrayList<>();
+        namesMap.forEach((key, value) -> stringList.add(value));
+        System.out.println(stringList);
+    }
+}
